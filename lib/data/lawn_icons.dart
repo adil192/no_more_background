4cb6518335a3efafb7785ff578354940257c38a2
Adil Hanney <adilhanney@disroot.org>
2026-02-14 00:43:10 +0000

    feat: add some app display names for my Pixel Watch

--- lib/data/lawn_icons.dart
+++ lib/data/lawn_icons.dart
@@ -4,17 +4,31 @@ import 'package:flutter/services.dart';
 /// We aren't actually using any icons from lawnicons.
 abstract class LawnIcons {
   /// Maps the package name to its display name.
+  ///
+  /// This is prepopulated with some apps that aren't yet included in lawnicons.
+  /// Also see [_displayNamesOverrides] which fixes some incorrect app names.
+  ///
+  /// Feel free to submit a PR to add more. Keep it alphabetical.
   static final _displayNames = <String, String>{
-    // Some apps that aren't in lawnicons.
-    // Feel free to submit a PR to add more.
     'com.adilhanney.fan': 'Just Fan Noise',
     'com.adilhanney.ricochlime': 'Ricochlime',
     'com.adilhanney.timing': 'Timing Trainer',
+    'com.fitbit.ecg': 'Fitbit ECG App',
     'com.google.android.contactkeys': 'Android System Key Verifier',
+    'com.google.android.marvin.talkback': 'Android Accessibility Suite',
     'com.google.android.mosey': 'Quick Share extension',
+    'com.google.android.wearable.assistant': 'Google Gemini on Wear OS',
+    'com.google.android.wearable.healthservices': 'Health Services',
+    'com.google.android.wearable.media.sessions': 'Wear Media',
+    'com.google.android.wearable.pixel.pdms': 'Pixel Watch Management Service',
     'com.moving.movinglife': 'MovingLife',
+    'com.MustBeFun.GamesInOneWatchGames': 'Games In One - Watch Games',
     'com.pg.grooming.braun.ipl': 'Braun IPL',
     'org.hanney.adil.nonogram': 'Nonogram',
+  };
+  static final _displayNamesOverrides = <String, String>{
+    'com.android.shell': 'Shell', // was LSPosed
+    'com.google.android.gms': 'Google Play services', // was microG Settings
   };
 
   static Future<void> init() async {
@@ -33,6 +47,7 @@ abstract class LawnIcons {
       final displayName = match.group(2)!;
       _displayNames[packageName] = displayName;
     }
+    _displayNames.addAll(_displayNamesOverrides);
   }
 
   static String? getDisplayName(String packageName) =>
