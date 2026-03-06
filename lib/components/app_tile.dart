a6e38a29a010cbd2dde90a47d24a34f37a3178cf
Adil Hanney <adilhanney@disroot.org>
2026-03-06 14:36:24 +0000

    ref: switch context menu to another package

--- lib/components/app_tile.dart
+++ lib/components/app_tile.dart
@@ -1,6 +1,5 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_context_menu/flutter_context_menu.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:no_more_background/compute/adb.dart';
 import 'package:no_more_background/data/adb_app.dart';
@@ -8,6 +7,7 @@ import 'package:no_more_background/data/adb_permissions.dart';
 import 'package:no_more_background/data/app_stores.dart';
 import 'package:no_more_background/data/reviewed_app.dart';
 import 'package:no_more_background/data/stows.dart';
+import 'package:super_context_menu/super_context_menu.dart';
 import 'package:yaru/yaru.dart';
 
 class AppTile extends StatefulHookWidget {
@@ -128,17 +128,19 @@ class _AppTileState extends State<AppTile> {
     if (mounted) setState(() {});
   }
 
-  late final _contextMenu = ContextMenu(
-    entries: [
-      MenuItem(
-        label: Builder(
-          builder: (context) =>
-              Text(widget.app.isUninstalled ? 'Request unarchive' : 'Archive'),
+  Menu? _menuProvider([MenuRequest? _]) {
+    if (widget.permissions == null) return null;
+
+    return Menu(
+      title: widget.app.displayName,
+      children: [
+        MenuAction(
+          title: widget.app.isUninstalled ? 'Request unarchive' : 'Archive',
+          callback: _toggleArchived,
         ),
-        onSelected: (_) => _toggleArchived(),
-      ),
-    ],
-  );
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -169,8 +171,8 @@ class _AppTileState extends State<AppTile> {
     return MouseRegion(
       onEnter: (_) => hovered.value = true,
       onExit: (_) => hovered.value = false,
-      child: ContextMenuRegion(
-        contextMenu: _contextMenu,
+      child: ContextMenuWidget(
+        menuProvider: _menuProvider,
         child: ColoredBox(
           // opaque so context menu gestures work
           color: theme.colorScheme.surface,
