8962b48d244297a26d4e74698f5161ecf9ce4252
Adil Hanney <adilhanney@disroot.org>
2026-03-12 22:57:10 +0000

    feat: add android platform files

---
+++ android/app/src/main/kotlin/com/adilhanney/no_more_background/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.adilhanney.no_more_background
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- scripts/generate_icons.sh
+++ scripts/generate_icons.sh
@@ -13,8 +13,37 @@ for svg_file in assets/icon/*.svg; do
 done
 echo
 
-# Run icons_launcher script
-announce dart run icons_launcher:create
+# Use the flutter_launcher_icons package to generate icons as a starting point
+echo "Generating icons with flutter_launcher_icons..."
+dart run flutter_launcher_icons
+echo
+
+# Re-generate android icons because flutter_launcher_icons' are pixelated
+echo "Replacing android icons"
+function replace_android_drawables() {
+  local size=$1
+  local sizeName=$2
+  echo "Resizing icon_android_fg.svg to ${sizeName} (${size}x${size})"
+  rsvg-convert -o "android/app/src/main/res/drawable-${sizeName}/ic_launcher_foreground.png" assets/icon/icon_android_fg.svg -w $size -h $size
+  echo "Resizing icon_android_mono.svg to ${sizeName} (${size}x${size})"
+  rsvg-convert -o "android/app/src/main/res/drawable-${sizeName}/ic_launcher_monochrome.png" assets/icon/icon_android_mono.svg -w $size -h $size
+}
+replace_android_drawables 108 mdpi
+replace_android_drawables 162 hdpi
+replace_android_drawables 216 xhdpi
+replace_android_drawables 324 xxhdpi
+replace_android_drawables 432 xxxhdpi
+function replace_android_mipmap() {
+  local size=$1
+  local sizeName=$2
+  echo "Resizing icon.png to ${sizeName} (${size}x${size})"
+  rsvg-convert -o "android/app/src/main/res/mipmap-${sizeName}/ic_launcher.png" assets/icon/icon.svg -w $size -h $size
+}
+replace_android_mipmap 48 mdpi
+replace_android_mipmap 72 hdpi
+replace_android_mipmap 96 xhdpi
+replace_android_mipmap 144 xxhdpi
+replace_android_mipmap 192 xxxhdpi
 echo
 
 echo "All done!"
