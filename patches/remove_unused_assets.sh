b1ffbdd1e13f4e6583f715ba8180b9cd0058af90
Adil Hanney <adilhanney@disroot.org>
2026-04-19 17:39:11 +0100

    fix: adapt patch to new Delta-Icons

--- patches/remove_unused_assets.sh
+++ patches/remove_unused_assets.sh
@@ -3,4 +3,4 @@
 # Removes a handful of unused assets from our submodules,
 # since Flutter doesn't offer that flexibility in pubspec.yaml.
 
-truncate -s 0 submodules/Delta-Icons/app/src/main/res/drawable-nodpi/ic_*.png
+truncate -s 0 submodules/Delta-Icons/app/src/main/res/drawable-nodpi/ic_*
