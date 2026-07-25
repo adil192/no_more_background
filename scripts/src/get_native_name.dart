3466b5d260ecf90ef7a9631fcdfff11f617196f3
Adil Hanney <adilhanney@disroot.org>
2026-07-25 23:50:42 +0100

    dev: add native name for Filipino

--- scripts/src/get_native_name.dart
+++ scripts/src/get_native_name.dart
@@ -2,6 +2,7 @@ import 'package:sealed_languages/sealed_languages.dart';
 
 String getNativeName(String localeCode) {
   return switch (localeCode) {
+    'fil' => 'Pilipino',
     'zh-Hans' => '中文（简体）',
     'zh-Hant' => '中文（繁體）',
     _ => NaturalLanguage.fromAnyCode(localeCode).namesNative.first,
